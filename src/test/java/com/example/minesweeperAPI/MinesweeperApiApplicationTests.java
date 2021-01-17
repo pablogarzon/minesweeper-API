6ae34add18ae8960dc405243e7263ada40bc43e9
pablo garzon <pablofgarzon@gmail.com>
2021-01-17 20:29:44 -0300

    initial commit

---
+++ src/test/java/com/example/minesweeperAPI/MinesweeperApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.minesweeperAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinesweeperApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
